2f6c62258e290390e56e1703ba4c4b2789a06b41
mukie <any_email_here>
2026-02-23 21:12:18 +0800

    observation_update

--- wan_va/configs/va_robotwin_i2va.py
+++ wan_va/configs/va_robotwin_i2va.py
@@ -5,7 +5,8 @@ from .va_robotwin_cfg import va_robotwin_cfg
 va_robotwin_i2va_cfg = EasyDict(__name__='Config: VA robotwin i2va')
 va_robotwin_i2va_cfg.update(va_robotwin_cfg)
 
-va_robotwin_i2va_cfg.input_img_path = 'example/robotwin'
+va_robotwin_i2va_cfg.input_img_path = 'example/hammer1'
 va_robotwin_i2va_cfg.num_chunks_to_infer = 10
-va_robotwin_i2va_cfg.prompt = 'Grab the medium-sized white mug, rotate it, place it on the table, and hook it onto the smooth dark gray rack.'
+va_robotwin_i2va_cfg.prompt = 'pick up the black water cup, and put it beside the blue block'
 va_robotwin_i2va_cfg.infer_mode = 'i2va'
+va_robotwin_i2va_cfg.output_video_name = 'hammer_demo1.mp4' 
